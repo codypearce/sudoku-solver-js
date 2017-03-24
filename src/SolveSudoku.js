ff33f5f7d4e09b36e18ec8434682446f52254259
codypearce <outis161@gmail.com>
2017-03-23 21:54:53 -0600

    solve function, set up check

--- src/SolveSudoku.js
+++ src/SolveSudoku.js
@@ -1,6 +1,25 @@
 class SolveSudoku {
     constructor(grid) {
-        console.log(grid);
+        this.grid = grid;
+        this.init();
+    }
+    init() {
+        this.solve(0,0);
+    }
+    solve(row, column) {
+        for(let num = 1; num <= 9; num++) {
+            if(this.check(row, column, num)) {
+                continue;
+            }
+            this.grid[row][column] = num;
+            if(this.solve((row, column))) {
+                return true;
+            }
+            this.grid[row][column] = 'X';
+        }
+    }
+    check() {
+        
     }
 }
 
