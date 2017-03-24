33e9e7cdbeee57998ac5381c7b4b4f6d44ef65ba
codypearce <outis161@gmail.com>
2017-03-23 22:05:08 -0600

    next cell returns array, add comments to explain solve

--- src/SolveSudoku.js
+++ src/SolveSudoku.js
@@ -4,26 +4,43 @@ class SolveSudoku {
         this.init();
     }
     init() {
-        this.solveCell(0,0);
+        const cell = [0, 0];
+        this.solveCell(cell);
     }
-    solveCell(row, column) {
-        var cell = this.nextCell();
-        row = cell[0];
-        column = cell[0];
+    solveCell(cell) {
+        const row = cell[0];
+        const column = cell[1];
         
         for(let num = 1; num <= 9; num++) {
-            if(this.check(row, column, num)) {
+            // If number is not unique in row, column, and box, then try next number
+            if(!this.check(row, column, num)) {
                 continue;
+            } else {
+                // Else try number in that cell
+                this.grid[row][column] = num;
+                
+                // Recurse next cell, if it works then keep going
+                if(this.solveCell(this.nextCell(row, column))) {
+                    return true;
+                }
+                // Backtrack if no unique number could be found
+                this.grid[row][column] = 'X';
             }
-            this.grid[row][column] = num;
-            if(this.solveCell((row, column))) {
-                return true;
-            }
-            this.grid[row][column] = 'X';
+            
         }
     }
-    nextCell() {
-        
+    nextCell(row, column) {
+        // Base Case, on last cell
+        if(row == 8 && column == 8) {
+            return;
+        } else if(column == 8) {
+            // End of row, go to next row
+            column = 0;
+            row++;
+        } else {
+            column++;
+        }
+        return [row, column];
     }
     check() {
         
