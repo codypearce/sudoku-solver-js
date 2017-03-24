262a7454aa1234844a74627a06cd32b1038b43c6
codypearce <outis161@gmail.com>
2017-03-23 20:10:02 -0600

    set up eslint with gulp

---
+++ gulpfile.js
@@ -0,0 +1,9 @@
+const   gulp = require('gulp'),
+        eslint = require('gulp-eslint');
+
+gulp.task('lint', () => {
+    return gulp.src(['**/*.js','!node_modules/**'])
+        .pipe(eslint())
+        .pipe(eslint.format())
+        .pipe(eslint.failAfterError());
+});
