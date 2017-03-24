1e05c7b6141612bf780b8b5cc4062394ed24c1f6
codypearce <outis161@gmail.com>
2017-03-23 20:34:28 -0600

    mocha tests, set up with gulp and travisci

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,6 @@
 const   gulp = require('gulp'),
-        eslint = require('gulp-eslint');
+        eslint = require('gulp-eslint'),
+        mocha = require('gulp-mocha');
 
 gulp.task('lint', () => {
     return gulp.src(['**/*.js','!node_modules/**'])
@@ -7,3 +8,10 @@ gulp.task('lint', () => {
         .pipe(eslint.format())
         .pipe(eslint.failAfterError());
 });
+
+gulp.task('test', ['lint'], () => {
+    return gulp.src('/test/test.js', {read: false})
+        .pipe(mocha({reporter: 'spec'}));
+});
+
+gulp.task('default', ['test']);
---
+++ test/test.js
@@ -0,0 +1,12 @@
+/* eslint-disable no-unused-vars */
+/* eslint-disable no-undef */
+const   expect = require('chai').expect,
+        Index = require('../index.js');
+        
+describe('Sudoku Solver', function() {
+    describe('Test', function() {
+        it('should pass', function() {
+            expect(1).to.equal(1);
+        });
+    });
+});
