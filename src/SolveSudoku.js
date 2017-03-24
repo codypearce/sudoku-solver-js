e19a3a0c9ddbe36bceaa1a162dea469bb673c0a6
codypearce <outis161@gmail.com>
2017-03-23 21:56:52 -0600

    next cell function

--- src/SolveSudoku.js
+++ src/SolveSudoku.js
@@ -4,19 +4,26 @@ class SolveSudoku {
         this.init();
     }
     init() {
-        this.solve(0,0);
+        this.solveCell(0,0);
     }
-    solve(row, column) {
+    solveCell(row, column) {
+        var cell = this.nextCell();
+        row = cell[0];
+        column = cell[0];
+        
         for(let num = 1; num <= 9; num++) {
             if(this.check(row, column, num)) {
                 continue;
             }
             this.grid[row][column] = num;
-            if(this.solve((row, column))) {
+            if(this.solveCell((row, column))) {
                 return true;
             }
             this.grid[row][column] = 'X';
         }
+    }
+    nextCell() {
+        
     }
     check() {
         
